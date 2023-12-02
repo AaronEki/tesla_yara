874d95e55c5e6d9bd82b60086301293ac4c8b160
Aaron Connolly <94369490+AaronEki@users.noreply.github.com>
2023-12-02 15:10:47 +0000

    Update utf-16-convert.py

--- utf-16-convert.py
+++ utf-16-convert.py
@@ -1,1 +1,5 @@
+text = "text to be encoded here"
 
+#format and printing code as UTF-16 little endian
+utf16_bytes = text.encode('utf-16le')
+print(" ".join(format(byte, '02x') for byte in utf16_bytes))
